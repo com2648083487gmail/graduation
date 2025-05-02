161ea6a3df20facb6ffd7234cb33fa5fa7142d2d
huangxue0528 <com2648083487@gmail.com>
2025-05-03 00:51:14 +0800

    fix: set publicPath to / for Railway deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-    // 只在生产环境中设置 publicPath，避免开发环境出错
-    publicPath: process.env.NODE_ENV === 'production'
-      ? '/graduation/' // 这里是你的 GitHub 仓库名
-      : '/'
+    publicPath: '/'
   }
