637f1125b6fe3f98ebbb02a771e0c7c92d3fcb74
huangxue0528 <com2648083487@gmail.com>
2025-05-02 23:00:11 +0800

    Initial commit

---
+++ src/mock/data.js
@@ -0,0 +1,63 @@
+export default {
+  code: 
+`
+// 今天毕业啦
+// 电商21-2给各位老师表演写代码
+// 我们要开始写啦
+const { 😆, 🤗 } = 电商21-2;
+😆.say('电商21-2全体成员申请毕业，望老师们批准!❤️');
+🤗.response('好呢，希望各位同学毕业之后一帆风顺！');
+Utils.graduate(😆,🤗);
+Utils.generateInvitation(😆,🤗);
+// 好了我写完了
+// 我要开始运行啦`,
+  executions: [{
+    name:'初始化',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'解压中',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'组装中',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'打包中',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'微笑🙂',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'大笑😄',
+    time: '',
+    duration: 0,
+    visible: false
+  }, {
+    name:'编译中...',
+    time: '',
+    visible: false
+  }],
+  barrages: [
+    '66666666',
+    '愿你美梦成真',
+    '你的邀请函真是美',
+    '人美心更美',
+    '你是最棒的',
+    '哇哇哇',
+    '还差xue生吗',
+    '一日为师终身为父',
+    '你写代码老快了',
+    '你的代码真是棒',
+    '😗 😙 😚 😋 ',
+    '只要代码写得好，工作永远少不了'
+  ],
+}
